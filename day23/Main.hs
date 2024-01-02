432ec322ef9e54b00ab839aefd4ce1adb3bd2e67
Vasil Shotarov <vshotarov@gmail.com>
2024-01-02 17:26:41 +0000

    add proper i.e. not waiting infinitely and testing what i get from the dfs solution to day 23 in haskell

--- day23/Main.hs
+++ day23/Main.hs
@@ -4,8 +4,6 @@ import qualified Common
 import qualified Data.Map as M
 import qualified Data.Set as S
 
-import Debug.Trace
-
 main :: IO ()
 main = do
     putStrLn $ "-- Solving day23 --"
@@ -16,32 +14,54 @@ main = do
     putStrLn $ "Parsed input: " ++ (Common.truncateString $ show parsedInput)
 
     -- Solve
-    let answer1 = walk parsedInput
-    let answer2 = "not solved yet"
+    let (maxX,maxY) = (maximum . map fst $ M.keys parsedInput,
+                       maximum . map snd $ M.keys parsedInput)
+    let answer1 = bfs (maxX-1,maxY) $ condense $ buildGraph False parsedInput --bfs (maxX-1,maxY) $ condense $ buildGraph False parsedInput
+    let answer2 = bfs (maxX-1,maxY) $ condense $ buildGraph True parsedInput --bfs (maxX-1,maxY) $ condense $ buildGraph True parsedInput
 
     -- Print answers
     putStrLn $ "Part 1: " ++ show answer1
     putStrLn $ "Part 2: " ++ show answer2
 
-parse :: String -> M.Map (Int,Int) Char
-parse input = M.fromList . concat $ zipWith (\y -> zipWith (\x c -> ((x,y),c)) [0..]) [0..] $ lines input
+type Pos = (Int,Int)
+type Grid = M.Map Pos Char
+type Graph = M.Map Pos [(Pos,Int)]
 
-walk :: M.Map (Int,Int) Char -> Int
-walk grid = go 0 [(S.empty, (1,0))]
-    where (maxX,maxY) = (maximum . map fst $ M.keys grid, maximum . map snd $ M.keys grid)
-          get = (grid M.!)
-          go best [] = best
-          go best ((path,p@(x,y)):explore)
-            | x < 0 || x > maxX || y < 0 || y > maxY
-                    || get p == '#' || p `S.member` path = go best explore
-            | p == (maxX-1, maxY) && S.size path > best = trace (show (S.size path)) $ go (max best $ S.size path) explore
-            | p == (maxX-1, maxY) = go (max best $ S.size path) explore
-            | otherwise = let path' = S.insert p path
-                              ns = case get p of
-                                     '>' -> [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
-                                     'v' -> [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
-                                     '.' -> [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
-                                     _   -> []
-                              --explore' = explore ++ (map (\n -> (path',n)) ns)
-                              explore' = (map (\n -> (path',n)) ns) ++ explore
-                           in go best explore'
+parse :: String -> Grid
+parse input = M.fromList . concat $ zipWith mapRow [0..] $ lines input
+    where mapRow y = zipWith (\x c -> ((x,y),c)) [0..]
+
+buildGraph :: Bool -> Grid -> Graph
+buildGraph ignoreSlopes grid = M.mapWithKey gridToGraph $ M.filter (/= '#') grid
+    where gridToGraph (x,y) c
+            | c == '.' || (c `elem` ">v" && ignoreSlopes) =
+                map (\n -> (n,1))
+              $ filter (\n -> M.findWithDefault '#' n grid /= '#')
+                       [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
+            | c == '>' = [((x+1,y),1)]
+            | c == 'v' = [((x,y+1),1)]
+            | otherwise = error "unreachable"
+
+bfs :: Pos -> Graph -> Int
+bfs goal graph = go S.empty 0 [(1,0,0)]
+    where go _ best [] = best
+          go seen best ((x,y,s):explore)
+            | s == -1                      = go (S.delete (x,y) seen) best explore
+            | (x,y) == goal                = go seen (max best s) explore
+            | S.member (x,y) seen          = go seen best explore
+            | otherwise                    =
+                          let seen' = S.insert (x,y) seen
+                              explore' = [(x,y,-1)] ++ explore
+                              explore'' = (map (\((nx,ny),d) -> (nx,ny,d+s)) (graph M.! (x,y))) ++ explore'
+                           in go seen' best explore''
+
+condense :: Graph -> Graph
+condense graph = let tunnels = M.toList $ M.filter ((==2) . length) graph
+                     ((x,y),[((x1,y1),s1),((x2,y2),s2)]) = head tunnels
+                     insertF new old = new ++ (filter ((/= (x,y)) . fst) old)
+                  in if null tunnels
+                        then graph
+                        else condense
+                           . M.insertWith insertF (x1,y1) [((x2,y2),s1+s2)]
+                           . M.insertWith insertF (x2,y2) [((x1,y1),s1+s2)]
+                           $ M.delete (x,y) graph
